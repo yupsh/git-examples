ff0df6c9660fd372ac3dca38b8a14cbcbaf1dfcc
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:03 -0800

    tests

--- command_test.go
+++ command_test.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"os"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	"github.com/yupsh/git"
-	. "github.com/gloo-foo/pipe"
 )
 
 func ExampleGit() {
 	// git status
 	// Note: This would execute actual git commands
-	if err := yup.Run(Pipeline(
+	if err := gloo.Run(Pipeline(
 		git.Git("status"),
 	)); err != nil {
 		fmt.Fprintf(os.Stderr, "git: %v\n", err)
